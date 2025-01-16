e68bfa6f6dd79d82198cae934956acb193bb16ee
Jay Nayanbhai Sanghani <jy483624@dal.ca>
2025-01-16 14:19:26 -0400

    Header file setup

--- job-portol/src/App.jsx
+++ job-portol/src/App.jsx
@@ -1,15 +1,13 @@
-import './css/App.css';
+// import './css/App.css';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import { Chip } from '@mantine/core';
+import Header from './components/header';
 
 function App() {
   return (
     <MantineProvider>
-      <h1 className="text-3xl text-blue-500 font-bold underline">
-        Hello world!
-      </h1>
-      <Chip defaultChecked>Awesome chip</Chip>
+      <Header/>
     </MantineProvider>
   );
 }
