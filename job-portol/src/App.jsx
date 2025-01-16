b1ad7181ce4093ff40034b007ac52196873dd874
Jay Nayanbhai Sanghani <jy483624@dal.ca>
2025-01-16 13:38:07 -0400

    Starting header

--- job-portol/src/App.jsx
+++ job-portol/src/App.jsx
@@ -1,12 +1,15 @@
+import './css/App.css';
 import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import { Chip } from '@mantine/core';
 
 function App() {
   return (
     <MantineProvider>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
-
+      <h1 className="text-3xl text-blue-500 font-bold underline">
+        Hello world!
+      </h1>
+      <Chip defaultChecked>Awesome chip</Chip>
     </MantineProvider>
   );
 }
